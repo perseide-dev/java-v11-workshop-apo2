a145df35faef55c82e864ea9e4a5502340e5cbf0
perseide-dev <miguelvg097@gmail.com>
2025-09-18 19:12:49 -0500

    Add skeleton for sub menu on simple linledlist

--- src/jdk_linkedlist/Jdk_linkedlist.java
+++ src/jdk_linkedlist/Jdk_linkedlist.java
@@ -7,6 +7,7 @@ public class Jdk_linkedlist {
     public static void main(String[] args) {
 
         int opt;
+        int opta;
 
         do {
             Scanner sc = new Scanner(System.in);
@@ -17,14 +18,72 @@ public class Jdk_linkedlist {
             System.out.println("2-Circula LinkedList");
             System.out.println("3-Double LinkedList");
             System.out.println("4-Circula Double LinkedList");
-            System.out.println("5-Salir");
+            System.out.println("5-Exit");
 
             opt = sc.nextInt();
             switch (opt) {
                 case 1:
-                    System.out.println("Simple linkedlist");
-                    List list1 = new List();
 
+                    do {
+                        Scanner sca = new Scanner(System.in);
+                        System.out.println("Simple linkedlist");
+                        System.out.println("List menu");
+                        System.out.println("Write an option");
+                        System.out.println("1-Push");
+                        System.out.println("2-Shift");
+                        System.out.println("3-Insert Max");
+                        System.out.println("4-Insert Min");
+                        System.out.println("5-Insert");
+                        System.out.println("6-unShift");
+                        System.out.println("7-Pop");
+                        System.out.println("8-Delete");
+                        System.out.println("9-Exit");
+
+                        opta = sca.nextInt();
+
+                        switch (opta) {
+                            case 1:
+                                System.out.println("Push");
+                                break;
+
+                            case 2:
+                                System.out.println("Shift");
+                                break;
+
+                            case 3:
+                                System.out.println("Insert Max");
+                                break;
+
+                            case 4:
+                                System.out.println("Insert Min");
+                                break;
+
+                            case 5:
+                                System.out.println("Insert");
+                                break;
+
+                            case 6:
+                                System.out.println("Unshift");
+                                break;
+
+                            case 7:
+                                System.out.println("Pop");
+                                break;
+
+                            case 8:
+                                System.out.println("Delete");
+                                break;
+
+                            case 9:
+                                System.out.println("Go to main menu :3");
+                                break;
+                                
+                            default:
+                                System.out.println("you write correct option");
+                                break;
+                        }
+
+                    } while (opta != 9);
                     break;
                 case 2:
                     System.out.println("option 1");
